2162f1afae0e1f2399ad6bac518276e7eef65a71
Noura <culurienneldoreth@gmail.com>
2013-10-31 23:48:55 -0700

    added comments to how the tutorial page works

--- static/js/tutorials.js
+++ static/js/tutorials.js
@@ -2,28 +2,36 @@
  * Noura Howell, October 2013
  *
  * Currently this just manages scrolling through the steps of the tutorial,
- * keeping in sync with the steps listed in the sidebar. The URL hash represents
- * which step is scrolled to. I intentionally did not make the URL hash match
- * the ID of HTML elements because this did not give the behavior I wanted. If 
- * the URL hash matches the ID of HTML elements on the page, then changing the 
- * URL hash will automatically jump scroll to that element. I want to update the
- * URL hash while the user is scrolling to match the step they have scrolled to,
- * without causing the jerky feeling of jumping around when changing the hash.
+ * keeping in sync with the steps listed in the sidebar. As the page scrolls,
+ * the hash in the URL and the sidebar highlight update to match the currently
+ * visible step. Clicking on the sidebar will scroll the page to the chosen step
+ * which will also update the hash and sidebar highlight.
+ *
+ * I intentionally did not make the URL hash match the ID of HTML elements
+ * because this did not give the behavior I wanted. If the URL hash matches the
+ * ID of HTML elements on the page, then changing the URL hash will
+ * automatically jump scroll to that element. I want to update the URL hash
+ * while the user is scrolling to match the step they have scrolled to, without
+ * causing the jerky feeling of jumping around when changing the hash.
  *
  * Also, sorry about the ugly/verbose selectors and class names. I was rushing.
- * 
+ *
  */
 
 $(document).ready(function(){
     // save jQuery objects for efficiency
     var $window = $(window);
     var $steps = {};
-    _.each(steps, function(step, i) {
+    _.each(steps, function(step, i) { // steps variable is available from tutorials.html
+        // numbers the tutorial steps 1, 2, 3, ...
         $steps[i+1] = $('[data-step="'+(i+1)+'"]');
     });
 
+    // For debugging. In the console you can access variables attached to the window
     window.$steps = $steps;
 
+    // Scrolls the window to the right step in the tutorial
+    // hash should be in a format like '#stepX' where X is a step in the tutorial
     function scroll_to_hash(hash) {
         if (hash.length > 5 && hash.slice(0, 5) == "#step") {
             var n = parseInt(hash.slice(5));
@@ -32,7 +40,8 @@ $(document).ready(function(){
             }
         }
     }
-    
+
+    // highlights the appropriate step in the sidebar
     function highlight_sidebar(hash) {
         $('li.tutorial-step-li').removeClass('active');
         $('a.link-to-tutorial-step[href="'+hash+'"]').parent('li.tutorial-step-li').addClass('active');
@@ -42,12 +51,12 @@ $(document).ready(function(){
     scroll_to_hash(window.location.hash);
     highlight_sidebar(window.location.hash);
 
-    // clicking a tutorial step link will automatically change the hash
-    // and this will scroll to the right part of the document
+    // clicking the sidebar will change the hash, scroll to that step, and
+    // update the sidebar highlight
     $('a.link-to-tutorial-step').on('click', function(ev) {
         var hash = $(this).attr('href');
         scroll_to_hash(hash);
-        highlight_sidebar(hash); 
+        highlight_sidebar(hash);
     });
 
     // make the URL hash match the highest visible step
@@ -68,8 +77,12 @@ $(document).ready(function(){
             var hash = '#step' + current_step;
             window.location.hash = hash;
             highlight_sidebar(hash);
-        }       
+        }
     };
+    // Throttling a function makes it only execute at most once every interval,
+    // in this case at most once every 500ms. This is necessary for performance
+    // because it would be too expensive to execute our sync_hash function
+    // every time the 'scroll' event is fired.
     var sync_hash_throttled = _.throttle(sync_hash, 500);
     $window.on('scroll', sync_hash_throttled);
 });
